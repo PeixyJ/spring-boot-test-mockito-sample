fa611d74d413f245fd9160fe6616dbb9867705e1
peixinyi <peixy_j@163.com>
2024-01-05 16:25:09 +0800

    feat : 完成初始化例子

--- src/test/java/priv/peixinyi/sbtms/test/SampleTest.java
+++ src/test/java/priv/peixinyi/sbtms/test/SampleTest.java
@@ -8,6 +8,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import priv.peixinyi.sbtms.entity.IStudent;
 import priv.peixinyi.sbtms.service.IStudentService;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import java.util.List;
 
 /**
@@ -26,6 +29,15 @@ public class SampleTest {
         queryWrapper.eq(IStudent::getAge, 16);
         List<IStudent> students = iStudentService.list(queryWrapper);
         log.info("16岁学生数量:{}", students.size());
+        //插入一个学生
+        IStudent student = new IStudent();
+        student.setClassId(5);
+        student.setName("XX");
+        student.setAge(16);
+        iStudentService.save(student);
+        //这样假如没有重置缓存的话,会从缓存中取出来,导致测试失败
+        students = iStudentService.list(queryWrapper);
+        assertEquals(29, students.size());
     }
 
 
